ac3ebfb5c6b785c0ebc39df511cdcd6d0f34d333
kieeo <18939003@qq.com>
2017-06-01 07:29:04 +0800

    create category and archive

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,13 @@ app_name='blog'
 urlpatterns =[
     url(r'^$',views.index,name='index'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.archives, name='archives'),
+        # 这个归档视图对应的 URL 的正则表达式和 detail 视图函数对应的 URL 是类似的，
+        # 这在之前我们讲过。两个括号括起来的地方是两个命名组参数，Django 会从用户访问的
+        # URL 中自动提取这两个参数的值，然后传递给其对应的视图函数。例如如果用户想查看
+        # 2017 年 3 月下的全部文章，他访问 /archives/2017/3/，
+        # 那么 archives 视图函数的实际调用为：archives(request, year=2017, month=3)。
+    url(r'^category/(?P<pk>[0-9]+)/$', views.category, name='category'),
+
 ]
+
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
-from .models import Post
+from .models import Post,Category
 import markdown
 
 """
@@ -32,3 +32,15 @@ def detail(request, pk):
                                       'markdown.extensions.toc',
                                   ])
     return render(request, 'blog/detail.html', context={'post': post})
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def category(request, pk):
+    # 记得在开始部分导入 Category 类
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
