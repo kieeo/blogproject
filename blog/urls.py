ec6c517af14cd807854847b9c8a02ae07e108ef6
kieeo <18939003@qq.com>
2017-06-01 16:44:31 +0800

    create comments22

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name='blog'
 urlpatterns =[
     url(r'^$',views.index,name='index'),
+    url(r'^index$',views.index,name='index'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
     url(r'^archives/(?P<year>[0-9]{4})/(?P<month>[0-9]{1,2})/$', views.archives, name='archives'),
         # 这个归档视图对应的 URL 的正则表达式和 detail 视图函数对应的 URL 是类似的，
